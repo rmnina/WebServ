d8d91e3074e76748be4a37539a9a2ff4a424ccb8
Elsa TOUMA <eltouma@bess-f2r5s16.clusters.42paris.fr>
2025-01-20 19:35:48 +0100

    print data

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 01:12:29 by jdufour           #+#    #+#             */
-/*   Updated: 2024/11/28 18:37:35 by jdufour          ###   ########.fr       */
+/*   Updated: 2025/01/20 11:52:44 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,21 @@ int	main(void)
 	std::string	filename = "test.conf";
 	Config	config;
 
-	config.fill_conf_vector(filename);
-	Handler	handler(config.get_servers_conf());
-	
-	try 
-	{
-		// printConfig(handler.get_servers_conf());
-		handler.loadServ();
-		handler.launchServers();
-		if (!handler.handleEvents())
-			return (1);
+	try {
+		config.fill_conf_vector(filename);
+		Handler	handler(config.get_servers_conf());	
+		try 
+		{
+			// printConfig(handler.get_servers_conf());
+			handler.loadServ();
+			handler.launchServers();
+			if (!handler.handleEvents())
+				return (1);
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
 	catch (std::exception &e)
 	{
