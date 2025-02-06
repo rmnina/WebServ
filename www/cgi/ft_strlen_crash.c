07d89fafd1c1c762fa4057ba179272f322dfb530
Elsa TOUMA <eltouma@bess-f3r4s3.clusters.42paris.fr>
2025-02-06 17:53:25 +0100

    Fix conditionnal jump with cgi. Edit Mime-type for .cpp and .c

---
+++ www/cgi/ft_strlen_crash.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	while (str[i] !='\0')
+}
+*/
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 2)
+		return (1);
+	while (*argv[i])
+		i += 1;
+	printf("length: %d\n", i);
+	return (i);
+}
